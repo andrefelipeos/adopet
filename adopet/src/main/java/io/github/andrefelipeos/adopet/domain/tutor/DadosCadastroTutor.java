3ea0e4b099f95e31276f66bfb13c3d66ca8d6c10
André Felipe <andre.felipe.2048@gmail.com>
2023-04-01 20:23:31 -0300

    feat: add validation constraints to record 'DadosCadastroTutor'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/domain/tutor/DadosCadastroTutor.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/tutor/DadosCadastroTutor.java
@@ -1,10 +1,19 @@
 package io.github.andrefelipeos.adopet.domain.tutor;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record DadosCadastroTutor(
+		@NotBlank
 		String nome,
+		@NotBlank
+		@Email
 		String email,
+		@NotBlank
 		String telefone,
+		@NotBlank
 		String cidade,
 		String biografia,
+		@NotBlank
 		String senha
 ) {}
