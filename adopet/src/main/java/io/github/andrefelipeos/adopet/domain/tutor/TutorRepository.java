999d348df5a583f37eb7683622c662ba8d56e1aa
André Felipe <andre.felipe.2048@gmail.com>
2023-04-01 20:05:51 -0300

    feat: create repository interface 'TutorRepository'

---
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/tutor/TutorRepository.java
@@ -0,0 +1,7 @@
+package io.github.andrefelipeos.adopet.domain.tutor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorRepository extends JpaRepository<Tutor, Long> {
+
+}
