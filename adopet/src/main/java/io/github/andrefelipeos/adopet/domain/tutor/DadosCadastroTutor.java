f36b1c16a1de8fa89b632b2b885d54a8c9639053
André Felipe <andre.felipe.2048@gmail.com>
2023-04-01 19:53:24 -0300

    feat: create record 'DadosCadastroTutor'

---
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/tutor/DadosCadastroTutor.java
@@ -0,0 +1,10 @@
+package io.github.andrefelipeos.adopet.domain.tutor;
+
+public record DadosCadastroTutor(
+		String nome,
+		String email,
+		String telefone,
+		String cidade,
+		String biografia,
+		String senha
+) {}
