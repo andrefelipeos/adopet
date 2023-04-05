7cdee495fadff2be4243c941eedc95c087f24349
André Felipe <andre.felipe.2048@gmail.com>
2023-04-05 19:20:46 -0300

    feat: add HTTP DELETE method 'excluir'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/controller/TutorController.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/controller/TutorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +54,12 @@ public class TutorController {
 		Tutor tutor = tutorRepository.getReferenceById(dados.identificador());
 		tutor.atualizarInformacoes(dados);
 		return ResponseEntity.ok(new DadosVisualizacaoTutor(tutor));
+	}
+
+	@DeleteMapping("/{identificador}")
+	@Transactional
+	public void excluir(@PathVariable Long identificador) {
+		tutorRepository.deleteById(identificador);
 	}
 
 }
