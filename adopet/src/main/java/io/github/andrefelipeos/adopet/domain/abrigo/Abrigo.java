2a82c68a77e1079b5f2c1566a3de5c40f2add6f6
André Felipe <andre.felipe.2048@gmail.com>
2023-04-07 12:38:24 -0300

    feat: create entity class 'Abrigo'

---
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/abrigo/Abrigo.java
@@ -0,0 +1,43 @@
+package io.github.andrefelipeos.adopet.domain.abrigo;
+
+import io.github.andrefelipeos.adopet.domain.endereco.Endereco;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Abrigo")
+@Table(name = "abrigos")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "identificador")
+@Getter
+public class Abrigo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long identificador;
+
+	private String nome;
+	private String email;
+	private String telefone;
+	private String descricao;
+
+	@Embedded
+	private Endereco endereco;
+
+	public Abrigo(DadosCadastroAbrigo dados) {
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.telefone = dados.telefone();
+		this.descricao = dados.descricao();
+		this.endereco = new Endereco(dados.endereco());
+	}
+
+}
