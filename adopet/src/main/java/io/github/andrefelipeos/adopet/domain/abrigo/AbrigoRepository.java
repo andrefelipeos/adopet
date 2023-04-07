6ceb6f1669f6a0bb2781e0442880c960a422aca6
André Felipe <andre.felipe.2048@gmail.com>
2023-04-07 13:02:54 -0300

    feat: create repository interface 'AbrigoRepository'

---
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/abrigo/AbrigoRepository.java
@@ -0,0 +1,7 @@
+package io.github.andrefelipeos.adopet.domain.abrigo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AbrigoRepository extends JpaRepository<Abrigo, Long> {
+
+}
