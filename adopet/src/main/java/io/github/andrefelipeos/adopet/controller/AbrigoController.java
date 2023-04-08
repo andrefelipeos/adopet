870a32fdbf5191263b2bf84be7e8b611682ad367
André Felipe <andre.felipe.2048@gmail.com>
2023-04-08 11:29:22 -0300

    feat: add HTTP GET method 'visualizar' to 'AbrigoController'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AbrigoController.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AbrigoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import io.github.andrefelipeos.adopet.domain.abrigo.Abrigo;
 import io.github.andrefelipeos.adopet.domain.abrigo.AbrigoRepository;
 import io.github.andrefelipeos.adopet.domain.abrigo.DadosCadastroAbrigo;
 import io.github.andrefelipeos.adopet.domain.abrigo.DadosListagemAbrigos;
+import io.github.andrefelipeos.adopet.domain.abrigo.DadosVisualizacaoAbrigo;
 import jakarta.validation.Valid;
 
 @RestController
@@ -35,6 +37,12 @@ public class AbrigoController {
 	public ResponseEntity<List<DadosListagemAbrigos>> listar() {
 		List<Abrigo> abrigos = abrigoRepository.findAll();
 		return ResponseEntity.ok(abrigos.stream().map(DadosListagemAbrigos::new).toList());
+	}
+
+	@GetMapping("/{identificador}")
+	public ResponseEntity<DadosVisualizacaoAbrigo> visualizar(@PathVariable Long identificador) {
+		Abrigo abrigo = abrigoRepository.getReferenceById(identificador);
+		return ResponseEntity.ok(new DadosVisualizacaoAbrigo(abrigo));
 	}
 
 }
