aac9a377b1d2d3abd8855ca4bcfb26b3ddd33479
André Felipe <andre.felipe.2048@gmail.com>
2023-04-09 03:54:38 -0300

    feat: add HTTP DELETE method 'excluir' to 'AbrigoController'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AbrigoController.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AbrigoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,12 @@ public class AbrigoController {
 	public ResponseEntity<DadosVisualizacaoAbrigo> visualizar(@PathVariable Long identificador) {
 		Abrigo abrigo = abrigoRepository.getReferenceById(identificador);
 		return ResponseEntity.ok(new DadosVisualizacaoAbrigo(abrigo));
+	}
+
+	@DeleteMapping("/{identificador}")
+	@Transactional
+	public void excluir(@PathVariable Long identificador) {
+		abrigoRepository.deleteById(identificador);
 	}
 
 }
