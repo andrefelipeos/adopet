2d0c410a31a24cc010b6fd5f78e5dc4d6aa96221
André Felipe <andre.felipe.2048@gmail.com>
2023-04-09 04:29:36 -0300

    feat: add HTTP PUT method 'atualizar' in 'AbrigoController'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AbrigoController.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AbrigoController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.andrefelipeos.adopet.domain.abrigo.Abrigo;
 import io.github.andrefelipeos.adopet.domain.abrigo.AbrigoRepository;
+import io.github.andrefelipeos.adopet.domain.abrigo.DadosAtualizacaoAbrigo;
 import io.github.andrefelipeos.adopet.domain.abrigo.DadosCadastroAbrigo;
 import io.github.andrefelipeos.adopet.domain.abrigo.DadosListagemAbrigos;
 import io.github.andrefelipeos.adopet.domain.abrigo.DadosVisualizacaoAbrigo;
@@ -43,6 +45,15 @@ public class AbrigoController {
 	@GetMapping("/{identificador}")
 	public ResponseEntity<DadosVisualizacaoAbrigo> visualizar(@PathVariable Long identificador) {
 		Abrigo abrigo = abrigoRepository.getReferenceById(identificador);
+		return ResponseEntity.ok(new DadosVisualizacaoAbrigo(abrigo));
+	}
+
+	@PutMapping("/{identificador}")
+	@Transactional
+	public ResponseEntity<DadosVisualizacaoAbrigo> atualizar(@PathVariable Long identificador,
+			@RequestBody @Valid DadosAtualizacaoAbrigo dados) {
+		Abrigo abrigo = abrigoRepository.getReferenceById(identificador);
+		abrigo.atualizarInformacoes(dados);
 		return ResponseEntity.ok(new DadosVisualizacaoAbrigo(abrigo));
 	}
 
