6d5a0761ac0dd576024aaa0a32ca7a833562f775
André Felipe <andre.felipe.2048@gmail.com>
2023-04-15 18:02:08 -0300

    feat: add field 'dataDeNascimento' in record 'DadosCadastroAnimal'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/domain/animal/DadosCadastroAnimal.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/animal/DadosCadastroAnimal.java
@@ -1,11 +1,15 @@
 package io.github.andrefelipeos.adopet.domain.animal;
 
+import java.time.LocalDate;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosCadastroAnimal(
 		@NotBlank
 		String nome,
+		@NotNull
+		LocalDate dataDeNascimento,
 		String descricao,
 		@NotNull
 		Boolean adotado
