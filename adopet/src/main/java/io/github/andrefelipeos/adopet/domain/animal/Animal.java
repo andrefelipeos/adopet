5936053ace2cadbfa18374436696f5131ce9b304
André Felipe <andre.felipe.2048@gmail.com>
2023-04-15 18:04:17 -0300

    feat: add field 'dataDeNascimento' in entity class 'Animal'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/domain/animal/Animal.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/animal/Animal.java
@@ -1,5 +1,9 @@
 package io.github.andrefelipeos.adopet.domain.animal;
 
+import java.time.LocalDate;
+import java.time.Period;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,10 +28,40 @@ public class Animal {
 	private String descricao;
 	private Boolean adotado;
 
+	@Column(name = "data_nasc")
+	private LocalDate dataDeNascimento;
+
 	public Animal(DadosCadastroAnimal dados) {
 		this.nome = dados.nome();
 		this.descricao = dados.descricao();
 		this.adotado = dados.adotado();
+		this.dataDeNascimento = dados.dataDeNascimento();
+	}
+
+	public String idadePorExtenso() {
+		StringBuilder idade = new StringBuilder();
+		int mesesTotais = Period.between(dataDeNascimento, LocalDate.now()).getMonths();
+		int anos = mesesTotais / 12;
+		int meses = mesesTotais % 12;
+		if (anos > 0) {
+			idade.append(anos);
+			idade.append(" ano");
+			if (anos > 1) {
+				idade.append("s");
+			}
+		}
+		if (meses > 0) {
+			idade.append(meses);
+			if (anos == 1) {
+				idade.append(" mês");
+			} else {
+				idade.append(" meses");
+			}
+		}
+		if (anos == 0 && meses == 0) {
+			idade.append("recém-nascido");
+		}
+		return null;
 	}
 
 }
