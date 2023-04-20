b07cd88b70e156558bfffa87ccf075ee3e0b4386
André Felipe <andre.felipe.2048@gmail.com>
2023-04-20 19:24:01 -0300

    refactor: save entity 'animal' with field 'abrigo'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AnimalController.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AnimalController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.andrefelipeos.adopet.domain.abrigo.Abrigo;
+import io.github.andrefelipeos.adopet.domain.abrigo.AbrigoRepository;
 import io.github.andrefelipeos.adopet.domain.animal.Animal;
 import io.github.andrefelipeos.adopet.domain.animal.AnimalRepository;
 import io.github.andrefelipeos.adopet.domain.animal.DadosCadastroAnimal;
@@ -20,10 +22,14 @@ public class AnimalController {
 	@Autowired
 	private AnimalRepository animalRepository;
 
+	@Autowired
+	private AbrigoRepository abrigoRepository;
+
 	@PostMapping
 	@Transactional
 	public ResponseEntity<Animal> cadastrar(@RequestBody @Valid DadosCadastroAnimal dados) {
-		Animal animal = animalRepository.save(new Animal(dados));
+		Abrigo abrigo = abrigoRepository.findById(dados.identificadorDoAbrigo()).get();
+		Animal animal = animalRepository.save(new Animal(dados, abrigo));
 		return ResponseEntity.ok(animal);
 	}
 
