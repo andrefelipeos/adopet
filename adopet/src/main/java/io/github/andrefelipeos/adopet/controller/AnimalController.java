4479719775539e4209b601287678880943b6b0bd
André Felipe <andre.felipe.2048@gmail.com>
2023-04-26 20:44:05 -0300

    feat: add HTTP GET method 'visualizar' to 'AnimalController'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AnimalController.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AnimalController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import io.github.andrefelipeos.adopet.domain.animal.Animal;
 import io.github.andrefelipeos.adopet.domain.animal.AnimalRepository;
 import io.github.andrefelipeos.adopet.domain.animal.DadosCadastroAnimal;
 import io.github.andrefelipeos.adopet.domain.animal.DadosListagemAnimais;
+import io.github.andrefelipeos.adopet.domain.animal.DadosVisualizacaoAnimal;
 import jakarta.validation.Valid;
 
 @RestController
@@ -41,6 +43,12 @@ public class AnimalController {
 	public ResponseEntity<List<DadosListagemAnimais>> listar() {
 		List<Animal> animais = animalRepository.findAll();
 		return ResponseEntity.ok(animais.stream().map(DadosListagemAnimais::new).toList());
+	}
+
+	@GetMapping("/{identificador}")
+	public ResponseEntity<DadosVisualizacaoAnimal> visualizar(@PathVariable Long identificador) {
+		Animal animal = animalRepository.getReferenceById(identificador);
+		return ResponseEntity.ok(new DadosVisualizacaoAnimal(animal));
 	}
 
 }
