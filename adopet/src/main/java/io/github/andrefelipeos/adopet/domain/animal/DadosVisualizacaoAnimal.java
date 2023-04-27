7bfe86bef3e41f107adc468670867a61b91cbcc2
André Felipe <andre.felipe.2048@gmail.com>
2023-04-27 19:09:17 -0300

    refactor: add field 'identificador' to 'DadosVisualizacaoAnimais'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/domain/animal/DadosVisualizacaoAnimal.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/animal/DadosVisualizacaoAnimal.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import io.github.andrefelipeos.adopet.domain.endereco.Endereco;
 
 public record DadosVisualizacaoAnimal(
+		Long identificador,
 		String nome,
 		Sexo sexo,
 		LocalDate dataDeNascimento,
@@ -13,7 +14,7 @@ public record DadosVisualizacaoAnimal(
 		String descricao) {
 
 	public DadosVisualizacaoAnimal(Animal animal) {
-		this(animal.getNome(), animal.getSexo(), animal.getDataDeNascimento(),
+		this(animal.getIdentificador(), animal.getNome(), animal.getSexo(), animal.getDataDeNascimento(),
 				animal.getAbrigo().getNome(), animal.getAbrigo().getEndereco(), animal.getDescricao());
 	}
 
