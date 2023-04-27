b2a9e8111e375c912b6d947803df24edca902e47
André Felipe <andre.felipe.2048@gmail.com>
2023-04-27 19:02:33 -0300

    feat: add HTTP DELETE method 'excluir' to 'AnimalController'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AnimalController.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AnimalController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,12 @@ public class AnimalController {
 	public ResponseEntity<DadosVisualizacaoAnimal> visualizar(@PathVariable Long identificador) {
 		Animal animal = animalRepository.getReferenceById(identificador);
 		return ResponseEntity.ok(new DadosVisualizacaoAnimal(animal));
+	}
+
+	@DeleteMapping("/{identificador}")
+	@Transactional
+	public void excluir(@PathVariable Long identificador) {
+		animalRepository.deleteById(identificador);
 	}
 
 }
