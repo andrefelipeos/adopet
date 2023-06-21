dc9d0793b1c6950df8a201657dd014e1bf02b8e1
André Felipe <andre.felipe.2048@gmail.com>
2023-06-20 22:20:37 -0300

    feat: create interface 'AdocaoRepository'

---
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/adocao/AdocaoRepository.java
@@ -0,0 +1,7 @@
+package io.github.andrefelipeos.adopet.domain.adocao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
+
+}
