da87b7a5e276e94348a135075a0b6bb5e7adbdea
André Felipe <andre.felipe.2048@gmail.com>
2023-06-20 22:19:33 -0300

    fix: set relationships cardinalities and columns names

--- adopet/src/main/java/io/github/andrefelipeos/adopet/domain/adocao/Adocao.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/adocao/Adocao.java
@@ -4,10 +4,14 @@ import java.time.LocalDate;
 
 import io.github.andrefelipeos.adopet.domain.animal.Animal;
 import io.github.andrefelipeos.adopet.domain.tutor.Tutor;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,10 +26,17 @@ public class Adocao {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long identificador;
 
 	private LocalDate data;
+
+	@OneToOne
+	@JoinColumn(name = "animal_id")
 	private Animal animal;
+
+	@OneToOne
+	@JoinColumn(name = "tutor_id")
 	private Tutor tutor;
 
 }
