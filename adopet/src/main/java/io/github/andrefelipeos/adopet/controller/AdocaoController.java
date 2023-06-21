677e59f56b0b1d0cb12062184cdae6d862321646
André Felipe <andre.felipe.2048@gmail.com>
2023-06-20 22:22:30 -0300

    feat: create controller class 'AdocaoController'

---
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/controller/AdocaoController.java
@@ -0,0 +1,44 @@
+package io.github.andrefelipeos.adopet.controller;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.andrefelipeos.adopet.domain.adocao.Adocao;
+import io.github.andrefelipeos.adopet.domain.adocao.AdocaoRepository;
+import io.github.andrefelipeos.adopet.domain.adocao.DadosCadastroAdocao;
+import io.github.andrefelipeos.adopet.domain.animal.Animal;
+import io.github.andrefelipeos.adopet.domain.animal.AnimalRepository;
+import io.github.andrefelipeos.adopet.domain.tutor.Tutor;
+import io.github.andrefelipeos.adopet.domain.tutor.TutorRepository;
+
+@RestController
+@RequestMapping("/adocao")
+public class AdocaoController {
+
+	@Autowired
+	private AdocaoRepository adocaoRepository;
+
+	@Autowired
+	private AnimalRepository animalRepository;
+
+	@Autowired
+	private TutorRepository tutorRepository;
+
+	@PostMapping
+	@Transactional
+	public ResponseEntity<Adocao> cadastrar(@RequestBody DadosCadastroAdocao dados) {
+		Tutor tutor = tutorRepository.getReferenceById(dados.identificadorTutor());
+		Animal animal = animalRepository.getReferenceById(dados.identificadorAnimal());
+		Adocao adocao = new Adocao(null, LocalDate.from(dados.data()), animal, tutor);
+		adocaoRepository.save(adocao);
+		return ResponseEntity.ok(adocao);
+	}
+
+}
