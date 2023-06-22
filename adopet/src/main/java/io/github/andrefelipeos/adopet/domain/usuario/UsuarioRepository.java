d21960b5b6a46145e2664acb003de35bde5d657b
André Felipe <andre.felipe.2048@gmail.com>
2023-06-22 13:48:09 -0300

    feat: add method 'findByLogin' in 'UsuarioRepository'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/domain/usuario/UsuarioRepository.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/usuario/UsuarioRepository.java
@@ -1,7 +1,10 @@
 package io.github.andrefelipeos.adopet.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	UserDetails findByLogin(String login);
 
 }
