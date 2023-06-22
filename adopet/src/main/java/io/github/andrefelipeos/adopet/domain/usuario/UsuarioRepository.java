cbc70c8f9f8dd477da76677997f4ff89e22b9930
André Felipe <andre.felipe.2048@gmail.com>
2023-06-22 13:41:12 -0300

    feat: create repository interface 'UsuarioRepository'

---
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package io.github.andrefelipeos.adopet.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
