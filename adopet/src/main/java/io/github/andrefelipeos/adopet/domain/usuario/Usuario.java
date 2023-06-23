c915db5bbc3a558b89787758cccc61ee6c57c8ef
André Felipe <andre.felipe.2048@gmail.com>
2023-06-23 10:39:07 -0300

    feat: implement interface 'UserDetails' in class 'Usuario'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/domain/usuario/Usuario.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/usuario/Usuario.java
@@ -1,5 +1,12 @@
 package io.github.andrefelipeos.adopet.domain.usuario;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,7 +23,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,5 +33,33 @@ public class Usuario {
 
 	private String login;
 	private String senha;
+
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	public String getPassword() {
+		return senha;
+	}
+
+	public String getUsername() {
+		return login;
+	}
+
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	public boolean isEnabled() {
+		return true;
+	}
 
 }
