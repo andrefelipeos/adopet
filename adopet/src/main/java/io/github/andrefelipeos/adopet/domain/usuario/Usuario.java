dce683596e51b5accab4c35c562234b98bc5afdf
André Felipe <andre.felipe.2048@gmail.com>
2023-06-23 10:49:47 -0300

    fix: set columns names in entity class 'Usuario'

--- adopet/src/main/java/io/github/andrefelipeos/adopet/domain/usuario/Usuario.java
+++ adopet/src/main/java/io/github/andrefelipeos/adopet/domain/usuario/Usuario.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,7 +32,10 @@ public class Usuario implements UserDetails {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(name = "username")
 	private String login;
+
+	@Column(name = "password")
 	private String senha;
 
 	public Collection<? extends GrantedAuthority> getAuthorities() {
